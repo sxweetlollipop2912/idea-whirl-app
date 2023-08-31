96c68cb06c8d4165f4414d648548633f19222831
Hoai Nam <hoainamnguyennhu7@gmail.com>
2023-08-31 21:03:33 +0700

    update get random note

--- app/src/main/java/com/example/ideawhirl/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/ideawhirl/ui/home/HomeViewModel.kt
@@ -4,15 +4,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.ideawhirl.data.repo.NoteRepo
 import com.example.ideawhirl.model.Note
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.flatMapConcat
-import kotlinx.coroutines.flow.toList
-
-@OptIn(FlowPreview::class)
-suspend fun <T> Flow<List<T>>.flattenToList() =
-    flatMapConcat { it.asFlow() }.toList()
 
 class HomeViewModel(
     private val repository: NoteRepo
@@ -20,8 +11,15 @@ class HomeViewModel(
 
     val notes = repository.getAll()
 
-    suspend fun getRandomNote(): Note {
-        return notes.flattenToList().random()
+    suspend fun getRandomNote(): Note? {
+        var random_note: Note? = null
+        notes.collect { newNotes ->
+            if (newNotes.isEmpty()) {
+                random_note = null
+            }
+            random_note = newNotes.random()
+        }
+        return random_note
     }
 
     companion object {

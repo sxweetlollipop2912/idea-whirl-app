6aaaca16c5b05c0439df5474713ccb28e64b174e
Hoai Nam <hoainamnguyennhu7@gmail.com>
2023-09-05 16:58:30 +0700

    fix when merging with navigation

--- app/src/main/java/com/example/ideawhirl/MainActivity.kt
+++ app/src/main/java/com/example/ideawhirl/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     this,
                     LocalDatabase::class.java,
                 ).fallbackToDestructiveMigration().build()
-                val noteRepo = NoteRepo(db)
+                val noteRepo = NoteRepo(db, this)
 
                 ThisNavGraph(
                     repository = noteRepo,
